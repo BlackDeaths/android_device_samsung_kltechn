f6408c1e229ce0f7ee782267c26cc4c4ca1dfe10
BlackDeath <BlackDeaths@users.noreply.github.com>
2018-03-13 14:25:58 +0100

    lineage to rr

--- rr.mk
+++ rr.mk
@@ -16,9 +16,9 @@
 #
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/rr/config/common_full_phone.mk)
 
 $(call inherit-product, device/samsung/kltechn/full_kltechn.mk)
 
 PRODUCT_DEVICE := kltechn
-PRODUCT_NAME := lineage_kltechn
+PRODUCT_NAME := rr_kltechn
